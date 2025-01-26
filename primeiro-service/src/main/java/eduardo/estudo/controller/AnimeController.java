c869f25576090a4844a3f67419cf6668ab616710
Eduardo Fabri <eduardohfabri@gmail.com>
2025-01-26 17:14:11 -0300

    video09: threads virtuais, stacktrace e tomcat threads

--- primeiro-service/src/main/java/eduardo/estudo/controller/AnimeController.java
+++ primeiro-service/src/main/java/eduardo/estudo/controller/AnimeController.java
@@ -1,18 +1,26 @@
 package eduardo.estudo.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("v1/animes")
+@Slf4j
 public class AnimeController {
 
+    private static final Logger log = LoggerFactory.getLogger(AnimeController.class);
+
     @GetMapping
-    private List<String> listAll() {
+    public List<String> listAll() throws InterruptedException {
+        log.info(Thread.currentThread().getName());
+        TimeUnit.SECONDS.sleep(1);
         return List.of("Naruto", "One Piece", "Dragon Ball");
     }
 }
