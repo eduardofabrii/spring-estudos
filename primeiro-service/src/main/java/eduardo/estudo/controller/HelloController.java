abed7e1d7f3ea8aa6379e2081e8b7c85c1771a14
Eduardo Fabri <eduardohfabri@gmail.com>
2025-01-26 14:19:51 -0300

    video06: aprendendo requestmapping e servlet context

--- primeiro-service/src/main/java/eduardo/estudo/controller/HelloController.java
+++ primeiro-service/src/main/java/eduardo/estudo/controller/HelloController.java
@@ -1,4 +1,4 @@
-package eduardo.estudo.controllers;
+package eduardo.estudo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
